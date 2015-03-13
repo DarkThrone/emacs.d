45f4c51a8e0755b86b160ea1545b68284b026d91
Gerónimo Garcia <garciasgritta@gmail.com>
2015-03-13 13:41:04 -0300

    Updated tab size

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -3,5 +3,9 @@
 ;;; Code:
 (require 'init-nyan)
 
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
 (provide 'init-local)
 ;;; init-local.el ends here
